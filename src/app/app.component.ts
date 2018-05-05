3c6659ac2d573a2f6a7937bfc81e390e23b08644
yeilop44 <pc_ylopez@hotmail.com>
2018-05-04 20:21:29 -0500

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,13 +11,68 @@ export class AppComponent {
   title = 'Angular Google Maps Yeison lopez';
   @ViewChild('gmap') gmapElement: any;
   map: google.maps.Map;
+  currentLat: any;
+  currentLong: any;
+  marker: any;
+  positionFin: any;
 
   ngOnInit() {
     var mapProp = {
-      center: new google.maps.LatLng(18.5793, 73.8143),
-      zoom: 15,
+      center: new google.maps.LatLng(6.235304600000001, -75.60764019999999),
+      zoom: 10,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+	this.positionFin = new google.maps.LatLng(6.2779398, -75.57198629999999)
   }
+
+
+setMapType(mapTypeId: string) {
+    this.map.setMapTypeId(mapTypeId)    
+}
+
+
+findMe() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.showPosition(position);
+        this.showPositionFin();
+      });
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+    
+  }
+
+  showPosition(position) {
+    let location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    this.map.panTo(location);
+
+    if (!this.marker) {
+      this.marker = new google.maps.Marker({
+        position: location,
+        map: this.map,
+        title: 'Posición Actual'
+      });
+    }
+    else {
+      this.marker.setPosition(location);
+    }
+  }
+   
+   showPositionFin() {
+   	
+    let location = new google.maps.LatLng(6.2779398, -75.57198629999999);
+    this.map.panTo(location);
+
+    
+      this.marker = new google.maps.Marker({
+        position: location,
+        map: this.map,
+        title: 'Posición Final'
+      });
+    
+   this.marker.setPosition(location);
+  }
+
 }
